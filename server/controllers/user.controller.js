d45220cfafbd201ccbb21275b9caed95ab406cbb
bethRefugio <bethrefugio16@gmail.com>
2025-10-02 12:21:43 +0800

    Update the banner, header and the send email message

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -47,7 +47,7 @@ export async function registerUserController(request,response){
 
         const verifyEmail = await sendEmail({
             sendTo : email,
-            subject : "Verify email from binkeyit",
+            subject : "Verify email from AgroLink",
             html : verifyEmailTemplate({
                 name,
                 url : VerifyEmailUrl
