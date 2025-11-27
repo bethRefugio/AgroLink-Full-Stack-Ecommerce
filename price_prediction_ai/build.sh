e7239f2a9d28028ef7a3ab63e32f26bf68a771b4
bethRefugio <bethrefugio16@gmail.com>
2025-11-27 23:17:16 +0800

    Fix build script path handling

--- price_prediction_ai/build.sh
+++ price_prediction_ai/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+# Change to script directory
+cd "$(dirname "$0")"
+
 echo "Installing Python dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
