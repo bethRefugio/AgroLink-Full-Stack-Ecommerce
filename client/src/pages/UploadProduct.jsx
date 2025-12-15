152941dcac21a7ec1ce2cbdfd2a69b4f2516e2fe
bethRefugio <bethrefugio16@gmail.com>
2025-12-15 19:42:45 +0800

    Remove unnecessary blank line in UploadProduct component

--- client/src/pages/UploadProduct.jsx
+++ client/src/pages/UploadProduct.jsx
@@ -961,7 +961,7 @@ const UploadProduct = () => {
           </div>
         </div>
       )}
-
+  
 {showSuggestionErrorModal && suggestionError && (
   <div className='fixed inset-0 bg-black/50 flex items-center justify-center z-50 p-4'>
     <div className='bg-white rounded-xl shadow-2xl w-full max-w-md p-6'>
